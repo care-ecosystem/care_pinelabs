52c68ac80dccbf364dd1721eda98674da15d204a
pranavrutagi-egov <pranav.rutagi@egovernments.org>
2026-07-29 16:37:43 +0530

    Refactor: Update PinelabsConfig model to include PaymentFlowChoices and rename fields for clarity

--- care_pinelabs/models/pinelabs_config.py
+++ care_pinelabs/models/pinelabs_config.py
@@ -4,10 +4,17 @@ from care.emr.models.base import EMRBaseModel
 from care_pinelabs.utils.encrypted_field import EncryptedCharField
 
 
+class PaymentFlowChoices(models.TextChoices):
+    PINELABS = "pinelabs", "Pinelabs"
+    NATIVE = "native", "Native"
+
+
 class PinelabsConfig(EMRBaseModel):
     facility = models.OneToOneField("facility.Facility", on_delete=models.CASCADE)
-    default_payment_flow = models.CharField(max_length=255, null=True, blank=True)
-    enable_advance = models.BooleanField(default=True)
-    enable_partial_payment = models.BooleanField(default=False)
+    default_payment_flow = models.CharField(
+        max_length=255, choices=PaymentFlowChoices.choices, null=True, blank=True
+    )
+    allow_advance_payment = models.BooleanField(default=True)
+    allow_partial_payment = models.BooleanField(default=False)
     pinelabs_merchant_id = models.CharField(max_length=255, null=False, blank=False)
     pinelabs_security_token = EncryptedCharField(null=False, blank=False)
