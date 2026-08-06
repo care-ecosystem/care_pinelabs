063fc233cff6534c11f1736d63ebd7b499f95113
Jagankumar E <53823168+jagankumar-egov@users.noreply.github.com>
2026-08-06 08:01:55 +0530

    Fix Deployment Update __init__.py to have all models (#30)

--- care_pinelabs/models/__init__.py
+++ care_pinelabs/models/__init__.py
@@ -1,9 +1,17 @@
+from care_pinelabs.models.pinelabs_config import PinelabsConfig
+from care_pinelabs.models.pinelabs_payment_method_mapping import (
+    PinelabsPaymentMethodMapping,
+)
+from care_pinelabs.models.pinelabs_pos_terminal import PinelabsPosTerminal
 from care_pinelabs.models.terminal_transaction import (
     PinelabsTerminalTransaction,
     TerminalTransactionStatus,
 )
 
 __all__ = [
+    "PinelabsConfig",
+    "PinelabsPaymentMethodMapping",
+    "PinelabsPosTerminal",
     "PinelabsTerminalTransaction",
     "TerminalTransactionStatus",
 ]
