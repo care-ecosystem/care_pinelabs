2babf311a0d50c3f4f666aebae24e853b9e78f35
Pranav Rustagi <pranav.rutagi@egovernments.org>
2026-08-06 17:40:59 +0530

    Enhance Pinelabs transaction model and API: add new fields and update filters (#35)

--- care_pinelabs/api/specs/pinelabs_transaction.py
+++ care_pinelabs/api/specs/pinelabs_transaction.py
@@ -1,8 +1,12 @@
 from datetime import datetime
+from decimal import Decimal
 
 from pydantic import UUID4
 
 from care.emr.resources.base import EMRResource
+from care.emr.resources.payment_reconciliation.spec import (
+    PaymentReconciliationOutcomeOptions,
+)
 from care_pinelabs.models.pinelabs_transaction import (
     PinelabsTransaction,
     PinelabsTransactionStatus,
@@ -16,9 +20,15 @@ class PinelabsTransactionReadSpec(EMRResource):
     id: UUID4 | None = None
     terminal: UUID4 | None = None
     transaction_number: str
-    status: PinelabsTransactionStatus
+    transaction_id: str | None = None
     method: str
     location: UUID4 | None = None
+    amount: Decimal | None = None
+    account: dict | None = None
+    target_invoice: dict | None = None
+    reference_number: str | None = None
+    tendered_amount: Decimal | None = None
+    status: PaymentReconciliationOutcomeOptions | None = None
     created_by: dict | None = None
     created_date: datetime
     modified_date: datetime
@@ -28,6 +38,7 @@ class PinelabsTransactionReadSpec(EMRResource):
         mapping["id"] = obj.external_id
         mapping["terminal"] = obj.terminal.external_id
         mapping["method"] = obj.payment_mode
+        mapping["transaction_id"] = obj.plutus_transaction_reference_id
 
         reconciliation = obj.payment_reconciliation
         mapping["location"] = (
@@ -35,6 +46,30 @@ class PinelabsTransactionReadSpec(EMRResource):
             if reconciliation and reconciliation.location
             else None
         )
+        mapping["amount"] = reconciliation.amount if reconciliation else None
+        mapping["account"] = (
+            {
+                "id": reconciliation.account.external_id,
+                "name": reconciliation.account.name,
+            }
+            if reconciliation and reconciliation.account
+            else None
+        )
+        mapping["target_invoice"] = (
+            {
+                "id": reconciliation.target_invoice.external_id,
+                "number": reconciliation.target_invoice.number,
+            }
+            if reconciliation and reconciliation.target_invoice
+            else None
+        )
+        mapping["reference_number"] = (
+            reconciliation.reference_number if reconciliation else None
+        )
+        mapping["tendered_amount"] = (
+            reconciliation.tendered_amount if reconciliation else None
+        )
+        mapping["status"] = reconciliation.outcome if reconciliation else None
         mapping["created_by"] = None
         if reconciliation and reconciliation.created_by_id:
             from care.emr.resources.base import model_from_cache
